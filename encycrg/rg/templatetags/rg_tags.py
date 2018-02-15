60b8ec030f4604535beedb20dac17ff366ada596
Geoff Froh <geoff.froh@densho.org>
2018-02-15 15:01:05 -0800

    templates: availability template tag in article sidebar

--- encycrg/rg/templatetags/rg_tags.py
+++ encycrg/rg/templatetags/rg_tags.py
@@ -34,3 +34,24 @@ def article(article):
         'fields': models.FACET_FIELDS,
     }
     return t.render(c)
+
+@register.inclusion_tag('rg/availabilitylevel-tag.html')
+def availabilitylevel(rawlevel):
+    """Availability level panel display
+    """
+    full = ""
+    empty = "123"
+    leveltext = "No availability (cannot currently be purchased or borrowed)"
+    if rawlevel[0].startswith('Limited'):
+        full = "1"
+        empty = "12"
+        leveltext = 'Limited availability (limited for purchase or expensive)'
+    if rawlevel[0].startswith('Available'):
+        full = "12"
+        empty = "1"
+        leveltext = 'Available (moderately easy to obtain)'
+    if rawlevel[0].startswith('Widely'):
+        full = "123"
+        empty = ""
+        leveltext = 'Widely available (easy to purchase or stream and reasonably priced/free)'
+    return {'leveltext':leveltext, 'full':full, 'empty':empty}
