ee266c49fceff41abba547309a0a352de2400cd0
Geoffrey Jost <geoffrey@jostwebwerks.com>
2020-02-19 16:24:51 -0800

    tests: Get multiple list,browse,search pages

--- encycrg/rg/tests.py
+++ encycrg/rg/tests.py
@@ -8,7 +8,12 @@ class APIView(TestCase):
         assert self.client.get(reverse('rg-api-index')).status_code == 200
 
     def test_articles(self):
-        assert self.client.get(reverse('rg-api-articles')).status_code == 200
+        data = {}
+        response = self.client.get(reverse('rg-api-articles'), data)
+        assert response.status_code == 200
+        data = {'offset': 25}
+        response = self.client.get(reverse('rg-api-articles'), data)
+        assert response.status_code == 200
 
     def test_article(self):
         assert self.client.get(
@@ -16,7 +21,12 @@ class APIView(TestCase):
         ).status_code == 200
 
     def test_authors(self):
-        assert self.client.get(reverse('rg-api-authors')).status_code == 200
+        data = {}
+        response = self.client.get(reverse('rg-api-authors'), data)
+        assert response.status_code == 200
+        data = {'offset': 25}
+        response = self.client.get(reverse('rg-api-authors'), data)
+        assert response.status_code == 200
 
     def test_author(self):
         assert self.client.get(
@@ -24,7 +34,12 @@ class APIView(TestCase):
         ).status_code == 200
 
     def test_sources(self):
-        assert self.client.get(reverse('rg-api-sources')).status_code == 200
+        data = {}
+        response = self.client.get(reverse('rg-api-sources'), data)
+        assert response.status_code == 200
+        data = {'offset': 25}
+        response = self.client.get(reverse('rg-api-sources'), data)
+        assert response.status_code == 200
 
     #def test_source(self):
     #    assert self.client.get(
@@ -39,10 +54,16 @@ class APIView(TestCase):
             reverse('rg-api-browse-field', args=['genre'])
         ).status_code == 200
 
-    def test_browse_field_value(self):
-        assert self.client.get(
-            reverse('rg-api-browse-fieldvalue', args=['genre', 'Art'])
-        ).status_code == 200
+    def test_browse_field_objects(self):
+        r = self.client.get(
+            reverse('rg-api-browse-fieldvalue', args=['genre', 'Art']), {}
+        )
+        assert r.status_code == 200
+        r = self.client.get(
+            reverse('rg-api-browse-fieldvalue', args=['genre', 'Art']),
+            {'limit': 25}
+        )
+        assert r.status_code == 200
 
     def test_search_index(self):
         data = {}
@@ -53,9 +74,15 @@ class APIView(TestCase):
         data = {'fulltext': 'camp'}
         response = self.client.get(reverse('rg-api-search'), data)
         assert response.status_code == 200
+        data = {'fulltext': 'camp', 'offset': 25}
+        response = self.client.get(reverse('rg-api-search'), data)
+        assert response.status_code == 200
 
     def test_search_fulltext_filter(self):
         data = {'fulltext': 'camp', 'rg_rgmediatype': 'books'}
+        response = self.client.get(reverse('rg-api-search'), data)
+        assert response.status_code == 200
+        data = {'fulltext': 'camp', 'rg_rgmediatype': 'books', 'offset': 25}
         response = self.client.get(reverse('rg-api-search'), data)
         assert response.status_code == 200
 
