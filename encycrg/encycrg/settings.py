8c4f3f11d2cdf3ba68bc00d60fd2f38d5bb618c7
Geoffrey Jost <geoffrey.jost@densho.org>
2026-08-28 08:11:08 -0700

    settings: Comment out logging mail_admins handler
    We do not want encycrg trying to mail admins with errors.

--- encycrg/encycrg/settings.py
+++ encycrg/encycrg/settings.py
@@ -397,12 +397,12 @@ LOGGING = {
             'filters': [],
             'formatter': 'verbose',
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-            'filters': ['require_debug_false'],
-            'formatter': 'verbose',
-        },
+        #'mail_admins': {
+        #    'level': 'ERROR',
+        #    'class': 'django.utils.log.AdminEmailHandler',
+        #    'filters': ['require_debug_false'],
+        #    'formatter': 'verbose',
+        #},
     },
     'loggers': {
         'django.request': {
